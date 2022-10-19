c841a129e17d22f6e109a0ae512e252b6e2cc500
Allison Silveira <allisonsilveiradev@gmail.com>
2022-10-19 18:21:32 -0300

    first commit

---
+++ components/navbar/Navbar.js
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const Bar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em 1.2em;
+`;
+
+const Logo = styled.h1`
+  font-family: "Cascadia";
+  span {
+    color: #7ac943;
+  }
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+const Item = styled.li`
+  margin-right: 3em;
+  cursor: pointer;
+`;
+
+export default function Navbar() {
+  return (
+    <Bar>
+      <Logo>
+        <span>{"{"}</span>
+        {"AllisonDev"}
+        <span>{"/}"}</span>
+      </Logo>
+      <Menu>
+        <Link href={"/projetos"}>
+          <Item>Projetos</Item>
+        </Link>
+
+        <Link href={"/habilidades"}>
+          <Item>Habilidades</Item>
+        </Link>
+
+        <Link href={"/sobre"}>
+          <Item>Sobre</Item>
+        </Link>
+
+        <Link href={"/contatos"}>
+          <Item>Contatos</Item>
+        </Link>
+      </Menu>
+    </Bar>
+  );
+}
---
+++ pages/projetos.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/navbar/Navbar.js";
+
+export default function Projetos() {
+  return (
+    <>
+      <Navbar />
+      <h1>Projetos</h1>
+    </>
+  );
+}
